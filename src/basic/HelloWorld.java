d85cf7f02a46daafffc969f432cfc14abbff056f
Jakub <kubastheballs@outlook.com>
2020-04-15 16:10:37 +0200

    dodany pakiet basic i plik helloworld

---
+++ src/basic/HelloWorld.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+        System.out.println("i'm adding code to my project");
+    }
+}
